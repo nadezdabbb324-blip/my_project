f53cc2600dabe89326e4eba015ff68a0bc16ba0e
надежда борисенко <nadezdabbb324@gmail.com>
2026-02-25 22:42:19 +1000

    Первый коммит

---
+++ вычисл мат лб1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <locale>
+#include <cmath>
+using namespace std;
+
+double f(double x) {
+    return pow((sin(x-8)),2) / 3 - (x - 2) / 5;
+}
+
+int main() {
+    setlocale(LC_ALL, "RUSSIAN");
+    cout << "решить уравнение: (sin(x-8))^2/3 - (x-2)/5 = 0" << endl;
+    //метод половинного деления
+    double a = 3.0;
+    double b = 4.0;
+    double e = 0.001;
+    int n=0;
+    while (b - a > 2 * e) {
+        n += 1;
+        double c = (a + b) / 2;
+        if (f(a) * f(c) < 0) {
+            b = c;
+        }
+        else {
+            a = c;
+        }
+    }
+    cout << "x~ = " << (a + b) / 2 << endl << "f(x~) = " << f((a + b) / 2) << endl<<
+        "n (количество итераций) = "<< n <<endl;
+}
